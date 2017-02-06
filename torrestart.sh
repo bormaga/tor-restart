c92a6a9cd460133108e2ae6e5a8d2d037777fa61
Mr Kara <hacker.alnofalia@gmail.com>
2017-02-06 09:15:02 +0300

    Update torrestart.sh

--- torrestart.sh
+++ torrestart.sh
@@ -1,4 +1,5 @@
-#mr crazy kara :v :v scripting in shell :v :3 :| 
+#!/bin/bash
+#bormaga :v :v scripting in shell :v :3 :| 
 sleep "3000"
 service tor restart
 echo -e "tor have been restarted in 3000 sec. xDDDD"
